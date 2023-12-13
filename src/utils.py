932b64b0ae0e2770b13d4643ce980800cf49c06c
FSadrieh <Frederic.Sadrieh@student.hpi.uni-potsdam.de>
2023-12-13 16:19:38 +0100

    V2 is done. Now supports init text and multiple models. Includes nicer code and new utils file to remove code duplication

---
+++ src/utils.py
@@ -0,0 +1,57 @@
+import torch
+
+
+def create_init_text(init_text: str, model_name: str, embedding_size: int, prompt_length: int) -> torch.Tensor:
+    """
+    This function mirrors the code in src/model.py to get the init text, to see how much the soft prompt changed
+    """
+    from transformers import AutoTokenizer
+    import math
+    from transformers.models.auto.modeling_auto import AutoModelForMaskedLM
+
+    model = AutoModelForMaskedLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+    soft_prompt = torch.nn.Embedding(prompt_length, embedding_size)
+
+    init_ids = tokenizer(init_text)["input_ids"]
+    if len(init_ids) > prompt_length:
+        init_ids = init_ids[:prompt_length]
+    elif len(init_ids) < prompt_length:
+        num_reps = math.ceil(prompt_length / len(init_ids))
+        init_ids = init_ids * num_reps
+    init_ids = init_ids[:prompt_length]
+    prompt_token_weights = model.get_input_embeddings()(torch.LongTensor(init_ids)).detach().clone()
+    soft_prompt.weight = torch.nn.Parameter(prompt_token_weights.to(torch.float32))
+    prompt_tokens = torch.arange(prompt_length).long()
+    return soft_prompt(prompt_tokens)
+
+
+def create_init_texts(init_texts: list, model_names: list, prompt_length: int, embedding_size: int) -> (list, list):
+    init_text_list = []
+    init_texts_names = []
+    for init_text in init_texts:
+        for model_name in model_names:
+            init_text_list.append(create_init_text(init_text, model_name, embedding_size, prompt_length))
+            init_texts_names.append(f"{init_text.split(' ')[:3]}_{model_name}")
+    return init_text_list, init_texts_names
+
+
+def create_soft_prompt(soft_prompt_name: str, prompt_length: int, embedding_size: int) -> torch.Tensor:
+    soft_prompt_path = f"logs/explainable-soft-prompts/{soft_prompt_name}/checkpoints/soft_prompt.pt"
+    soft_prompt = torch.nn.Embedding(prompt_length, embedding_size)
+    soft_prompt.load_state_dict(torch.load(soft_prompt_path))
+    prompt_tokens = torch.arange(prompt_length).long()
+    return soft_prompt(prompt_tokens)
+
+
+def create_soft_prompts(soft_prompt_names: list, prompt_length: int, embedding_size: int) -> list:
+    soft_prompt_list = []
+    for soft_prompt_name in soft_prompt_names:
+        soft_prompt_list.append(create_soft_prompt(soft_prompt_name, prompt_length, embedding_size))
+    return soft_prompt_list
+
+
+def average(soft_prompt_list: list, average: bool) -> (torch.Tensor, int):
+    if average:
+        return torch.cat([torch.mean(soft_prompt, dim=0) for soft_prompt in soft_prompt_list], dim=0), 1
+    return torch.cat(soft_prompt_list, dim=0), soft_prompt_list[0].shape[0]
--- src/visualizer.py
+++ src/visualizer.py
@@ -5,35 +5,41 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 from matplotlib import pyplot as plt
 
+from utils import create_soft_prompts, create_init_texts, average
+
 DEFAULT_COLORS = ["blue", "orange", "green", "red", "purple", "brown", "pink", "gray", "olive", "cyan"]
 
 
 def arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("soft_prompt_names", type=str)
-    parser.add_argument("model", type=str)
+    parser.add_argument("models", type=str)
     parser.add_argument("output_path", type=str)
+    parser.add_argument("-i", "--init_texts", type=str, default=None)
+    parser.add_argument(
+        "-m",
+        "--init_text_models",
+        type=str,
+        default=None,
+        help="Comma separated list of models to use for init text. If not specified the models for the embeddings will be used",
+    )
+    parser.add_argument("-a", "--average", action="store_true", default=False)
     parser.add_argument("-e", "--embedding_size", type=str, default=768)
     parser.add_argument("-p", "--prompt_length", type=str, default=30)
     return parser.parse_args()
 
 
-def create_soft_prompt(prompt_length: int, embedding_size: int, soft_prompt_name: str) -> torch.Tensor:
-    soft_prompt_path = f"logs/explainable-soft-prompts/{soft_prompt_name}/checkpoints/soft_prompt.pt"
-    soft_prompt = torch.nn.Embedding(prompt_length, embedding_size)
-    soft_prompt.load_state_dict(torch.load(soft_prompt_path))
-    prompt_tokens = torch.arange(prompt_length).long()
-    return soft_prompt(prompt_tokens)
-
-
-def get_model_embedding_space(model):
+def get_model_embedding_spaces(models: list) -> (torch.Tensor, list):
     from transformers.models.auto.modeling_auto import AutoModelForMaskedLM
 
-    model = AutoModelForMaskedLM.from_pretrained(model)
-    return model.get_input_embeddings().weight
+    embeddings = []
+    for model in models:
+        model = AutoModelForMaskedLM.from_pretrained(model)
+        embeddings.append(model.get_input_embeddings().weight)
+    return torch.cat(embeddings, dim=0), models
 
 
-def reduce_embedding_space(embedding_space, n_components=50):
+def reduce_embedding_space(embedding_space: torch.Tensor, n_components: int = 50) -> torch.Tensor:
     pca = PCA(n_components=n_components)
     reduced_embedding_space = pca.fit_transform(embedding_space)
 
@@ -42,52 +48,91 @@ def reduce_embedding_space(embedding_space, n_components=50):
 
 
 def plot_embedding_space(
-    embedding_space, output_path, embedding_space_size, prompt_length, prompts, model, colors=DEFAULT_COLORS
-):
+    embedding_space: torch.Tensor,
+    output_path: str,
+    embedding_space_size: int,
+    prompt_length: int,
+    prompts: list,
+    model_names: list,
+    colors: list = DEFAULT_COLORS,
+) -> None:
+    """
+    Plots the embedding space in steps. First the embedding spaces from the different models are plotted with a low alpha value. Then the prompts and init texts are plotted with a higher alpha value.
+    """
     fig, ax = plt.subplots(figsize=(10, 8))
-    ax.scatter(
-        embedding_space[:embedding_space_size, 0],
-        embedding_space[:embedding_space_size, 1],
-        alpha=0.1,
-        color=colors[0],
-        label=f"Embeddings from {model}",
-    )
-    lower_bound = embedding_space_size
+    lower_bound = 0
+    for i in range(len(model_names)):
+        upper_bound = lower_bound + embedding_space_size
+        ax.scatter(
+            embedding_space[lower_bound:upper_bound, 0],
+            embedding_space[lower_bound:upper_bound, 1],
+            alpha=0.1,
+            color=colors[i],
+            label=f"Embeddings from {model_names[i]}",
+        )
+        lower_bound = upper_bound
     for i in range(len(prompts)):
         upper_bound = lower_bound + prompt_length
         ax.scatter(
             embedding_space[lower_bound:upper_bound, 0],
             embedding_space[lower_bound:upper_bound, 1],
             alpha=1,
-            color=colors[i + 1],
-            label=f"Soft prompt {prompts[i]}",
+            color=colors[i + len(model_names)],
+            label=f"{prompts[i]}",
         )
         lower_bound = upper_bound
     ax.legend()
     fig.savefig(output_path)
 
 
+def prepare_soft_prompts(
+    soft_prompt_names: list, prompt_length: int, embedding_size: int, avg: bool
+) -> (torch.Tensor, list, int):
+    soft_prompt_list = create_soft_prompts(soft_prompt_names, prompt_length, embedding_size)
+    soft_prompts, prompt_length = average(soft_prompt_list, avg)
+    return soft_prompts, soft_prompt_names, prompt_length
+
+
+def prepare_init_texts(
+    init_texts: list,
+    model_names: list,
+    prompt_length: int,
+    embedding_size: int,
+    avg: bool,
+    soft_prompts: torch.Tensor,
+    soft_prompt_names: list,
+) -> (torch.Tensor, list):
+    if not init_texts:
+        return soft_prompts, soft_prompt_names
+    init_texts = init_texts.split(",")
+    init_texts_list, init_text_names = create_init_texts(init_texts, model_names, prompt_length, embedding_size)
+    init_texts, _ = average(init_texts_list, avg)
+    soft_prompt_names.extend(init_text_names)
+    return torch.cat([soft_prompts, init_texts], dim=0), soft_prompt_names
+
+
 def main():
     args = arg_parser()
 
-    soft_prompt_names = args.soft_prompt_names.split(",")
-    soft_prompt_list = []
-    # Creates a soft prompt for each soft prompt name
-    for soft_prompt_name in soft_prompt_names:
-        soft_prompt_list.append(create_soft_prompt(args.prompt_length, args.embedding_size, soft_prompt_name))
-    soft_prompts = torch.cat(soft_prompt_list, dim=0)
+    soft_prompts, soft_prompt_names, prompt_length = prepare_soft_prompts(
+        args.soft_prompt_names.split(","), args.prompt_length, args.embedding_size, args.average
+    )
+    init_models = args.init_text_models.split(",") if args.init_text_models else args.models.split(",")
+    soft_prompts, soft_prompt_names = prepare_init_texts(
+        args.init_texts, init_models, args.prompt_length, args.embedding_size, args.average, soft_prompts, soft_prompt_names
+    )
 
-    embeddings = get_model_embedding_space(args.model)
+    embeddings, model_names = get_model_embedding_spaces(args.models.split(","))
     embedding_space = torch.cat([embeddings, soft_prompts], dim=0).detach().numpy()
 
     reduced_embedding_space = reduce_embedding_space(embedding_space)
     plot_embedding_space(
         reduced_embedding_space,
         output_path=args.output_path,
-        embedding_space_size=embeddings.shape[0],
-        prompt_length=args.prompt_length,
+        embedding_space_size=embeddings.shape[0] // len(model_names),
+        prompt_length=prompt_length,
         prompts=soft_prompt_names,
-        model=args.model,
+        model_names=model_names,
     )
 
 
