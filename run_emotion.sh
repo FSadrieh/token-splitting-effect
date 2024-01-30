eaa9365f9cd3fca57dcab0587eb7b54ce99f0f43
FSadrieh <Frederic.Sadrieh@student.hpi.uni-potsdam.de>
2024-01-30 16:43:22 +0100

    Run emotion script

---
+++ run_emotion.sh
@@ -0,0 +1,18 @@
+python dlib/data/data_download.py emotion -o data
+
+python train.py --config cfgs/emotion/5_models/models/emotion_5_model_0.yml
+python train.py --config cfgs/emotion/5_models/models/emotion_5_model_1.yml
+python train.py --config cfgs/emotion/5_models/models/emotion_5_model_2.yml
+
+# These are the 5_models with different soft prompt initialization
+python train.py --config cfgs/emotion/5_models/init_seeds/emotion_5_model_init_1.yml
+python train.py --config cfgs/emotion/5_models/init_seeds/emotion_5_model_init_2.yml
+
+# These are the 5_models with different training seeds
+python train.py --config cfgs/emotion/5_models/train_seeds/emotion_5_model_train_1.yml
+python train.py --config cfgs/emotion/5_models/train_seeds/emotion_5_model_train_2.yml
+
+#10 models
+python train.py --config cfgs/emotion/10_models/emotion_10_model_0.yml
+python train.py --config cfgs/emotion/10_models/emotion_10_model_1.yml
+python train.py --config cfgs/emotion/10_models/emotion_10_model_2.yml
